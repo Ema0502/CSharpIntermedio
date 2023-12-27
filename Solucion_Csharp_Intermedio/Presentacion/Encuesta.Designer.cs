1f72785fbc47c1219cf8d7a3aa751ecb2342dea1
Ema0502 <ema050296@gmail.com>
2023-12-27 16:08:20 -0300

    checkbox + radio button

---
+++ Solucion_Csharp_Intermedio/Presentacion/Encuesta.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace Presentacion
+{
+    partial class Encuesta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Chbox_JavaScript = new System.Windows.Forms.CheckBox();
+            this.Chbox_CSharp = new System.Windows.Forms.CheckBox();
+            this.Chbox_NodeJs = new System.Windows.Forms.CheckBox();
+            this.Chbox_React = new System.Windows.Forms.CheckBox();
+            this.Chbox_TypeScript = new System.Windows.Forms.CheckBox();
+            this.Txt_Resultado = new System.Windows.Forms.TextBox();
+            this.Btn_Procesar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Rdb_Español = new System.Windows.Forms.RadioButton();
+            this.Rdb_Ingles = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Chbox_JavaScript
+            // 
+            this.Chbox_JavaScript.AutoSize = true;
+            this.Chbox_JavaScript.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Chbox_JavaScript.Location = new System.Drawing.Point(48, 130);
+            this.Chbox_JavaScript.Name = "Chbox_JavaScript";
+            this.Chbox_JavaScript.Size = new System.Drawing.Size(120, 24);
+            this.Chbox_JavaScript.TabIndex = 0;
+            this.Chbox_JavaScript.Text = "JavaScript";
+            this.Chbox_JavaScript.UseVisualStyleBackColor = true;
+            // 
+            // Chbox_CSharp
+            // 
+            this.Chbox_CSharp.AutoSize = true;
+            this.Chbox_CSharp.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Chbox_CSharp.Location = new System.Drawing.Point(48, 87);
+            this.Chbox_CSharp.Name = "Chbox_CSharp";
+            this.Chbox_CSharp.Size = new System.Drawing.Size(54, 24);
+            this.Chbox_CSharp.TabIndex = 1;
+            this.Chbox_CSharp.Text = "C#";
+            this.Chbox_CSharp.UseVisualStyleBackColor = true;
+            // 
+            // Chbox_NodeJs
+            // 
+            this.Chbox_NodeJs.AutoSize = true;
+            this.Chbox_NodeJs.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Chbox_NodeJs.Location = new System.Drawing.Point(48, 249);
+            this.Chbox_NodeJs.Name = "Chbox_NodeJs";
+            this.Chbox_NodeJs.Size = new System.Drawing.Size(92, 24);
+            this.Chbox_NodeJs.TabIndex = 2;
+            this.Chbox_NodeJs.Text = "NodeJs";
+            this.Chbox_NodeJs.UseVisualStyleBackColor = true;
+            // 
+            // Chbox_React
+            // 
+            this.Chbox_React.AutoSize = true;
+            this.Chbox_React.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Chbox_React.Location = new System.Drawing.Point(48, 209);
+            this.Chbox_React.Name = "Chbox_React";
+            this.Chbox_React.Size = new System.Drawing.Size(79, 24);
+            this.Chbox_React.TabIndex = 3;
+            this.Chbox_React.Text = "React";
+            this.Chbox_React.UseVisualStyleBackColor = true;
+            // 
+            // Chbox_TypeScript
+            // 
+            this.Chbox_TypeScript.AutoSize = true;
+            this.Chbox_TypeScript.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Chbox_TypeScript.Location = new System.Drawing.Point(48, 170);
+            this.Chbox_TypeScript.Name = "Chbox_TypeScript";
+            this.Chbox_TypeScript.Size = new System.Drawing.Size(123, 24);
+            this.Chbox_TypeScript.TabIndex = 4;
+            this.Chbox_TypeScript.Text = "TypeScript";
+            this.Chbox_TypeScript.UseVisualStyleBackColor = true;
+            // 
+            // Txt_Resultado
+            // 
+            this.Txt_Resultado.Location = new System.Drawing.Point(247, 230);
+            this.Txt_Resultado.MaxLength = 3276;
+            this.Txt_Resultado.Multiline = true;
+            this.Txt_Resultado.Name = "Txt_Resultado";
+            this.Txt_Resultado.ReadOnly = true;
+            this.Txt_Resultado.Size = new System.Drawing.Size(378, 202);
+            this.Txt_Resultado.TabIndex = 5;
+            // 
+            // Btn_Procesar
+            // 
+            this.Btn_Procesar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.Btn_Procesar.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_Procesar.Location = new System.Drawing.Point(48, 325);
+            this.Btn_Procesar.Name = "Btn_Procesar";
+            this.Btn_Procesar.Size = new System.Drawing.Size(122, 52);
+            this.Btn_Procesar.TabIndex = 6;
+            this.Btn_Procesar.Text = "Procesar";
+            this.Btn_Procesar.UseVisualStyleBackColor = false;
+            this.Btn_Procesar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Montserrat Alternates Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(243, 190);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(389, 22);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Resultados de los elementos seleccionados";
+            // 
+            // Rdb_Español
+            // 
+            this.Rdb_Español.AutoSize = true;
+            this.Rdb_Español.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Rdb_Español.Location = new System.Drawing.Point(266, 103);
+            this.Rdb_Español.Name = "Rdb_Español";
+            this.Rdb_Español.Size = new System.Drawing.Size(91, 26);
+            this.Rdb_Español.TabIndex = 8;
+            this.Rdb_Español.TabStop = true;
+            this.Rdb_Español.Text = "Español";
+            this.Rdb_Español.UseVisualStyleBackColor = true;
+            // 
+            // Rdb_Ingles
+            // 
+            this.Rdb_Ingles.AutoSize = true;
+            this.Rdb_Ingles.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Rdb_Ingles.Location = new System.Drawing.Point(452, 103);
+            this.Rdb_Ingles.Name = "Rdb_Ingles";
+            this.Rdb_Ingles.Size = new System.Drawing.Size(76, 26);
+            this.Rdb_Ingles.TabIndex = 9;
+            this.Rdb_Ingles.TabStop = true;
+            this.Rdb_Ingles.Text = "Ingles";
+            this.Rdb_Ingles.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Montserrat Alternates Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(25, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(158, 22);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Cursos Deseados";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Montserrat Alternates Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(395, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 22);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Idioma";
+            // 
+            // Encuesta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PowderBlue;
+            this.ClientSize = new System.Drawing.Size(662, 454);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Rdb_Ingles);
+            this.Controls.Add(this.Rdb_Español);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Btn_Procesar);
+            this.Controls.Add(this.Txt_Resultado);
+            this.Controls.Add(this.Chbox_TypeScript);
+            this.Controls.Add(this.Chbox_React);
+            this.Controls.Add(this.Chbox_NodeJs);
+            this.Controls.Add(this.Chbox_CSharp);
+            this.Controls.Add(this.Chbox_JavaScript);
+            this.Name = "Encuesta";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Encuesta";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox Chbox_JavaScript;
+        private System.Windows.Forms.CheckBox Chbox_CSharp;
+        private System.Windows.Forms.CheckBox Chbox_NodeJs;
+        private System.Windows.Forms.CheckBox Chbox_React;
+        private System.Windows.Forms.CheckBox Chbox_TypeScript;
+        private System.Windows.Forms.TextBox Txt_Resultado;
+        private System.Windows.Forms.Button Btn_Procesar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton Rdb_Español;
+        private System.Windows.Forms.RadioButton Rdb_Ingles;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ Solucion_Csharp_Intermedio/Presentacion/Encuesta.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Presentacion
+{
+    public partial class Encuesta : Form
+    {
+        public Encuesta()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string Resultado = "";
+
+            if (Chbox_CSharp.Checked == true)
+            {
+                Resultado += Chbox_CSharp.Text + ", ";
+            }
+            if (Chbox_JavaScript.Checked == true)
+            {
+                Resultado += Chbox_JavaScript.Text + ", ";
+            }
+            if (Chbox_NodeJs.Checked == true)
+            {
+                Resultado += Chbox_NodeJs.Text + ", ";
+            }
+            if (Chbox_React.Checked == true)
+            {
+                Resultado += Chbox_React.Text + ", ";
+            }
+            if (Chbox_TypeScript.Checked == true)
+            {
+                Resultado += Chbox_TypeScript.Text;
+            }
+
+            //seleccion radiobutton
+
+            if (Rdb_Ingles.Checked == true)
+            {
+                Resultado += ">>>>>>>" + Rdb_Ingles.Text + "<<<<<<<";
+            } 
+            else
+            {
+                Resultado += ">>>>>>>" + Rdb_Español.Text + "<<<<<<<";
+            }
+
+            Txt_Resultado.Text = Resultado;
+        }
+    }
+}
